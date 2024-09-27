6008a625a46c0623e56c26effbc2669b3c57638d
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-09-27 16:54:12 +0100

    Python setup

---
+++ python-scraper/scrape.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def fetch_books(page_number):
+    print(page_number)
+
+def main():
+    fetch_books(1)
+
+if __name__ == "__main__": 
+    main()
